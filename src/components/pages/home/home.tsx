f8135b37914f27728624dad2b5a1194e77d590e2
Lucas Medeiros <lucas-medeiros@live.com>
2023-02-19 17:17:28 -0300

    connectint to api

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import api from '../../../services/api'
+
 function home () {
+  const [moveis, setMovies] = useState([]);
+
+  useEffect(() =>{
+
+    async function loadMovies(){
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: '3e5e48c2fadb1d201ea994c146ebff5d',
+          language: "pt-br",
+          page: 1,
+        }
+      });
+
+      console.log(response.data.results);
+    }
+
+    loadMovies();
+  }, []);
   return (
     <div>
       <h1>Bem vindo a home</h1>
