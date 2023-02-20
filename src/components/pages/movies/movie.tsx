876b7d747ae5af36c515a00f75a964f7bee3dad9
Lucas Medeiros <lucas-medeiros@live.com>
2023-02-20 12:30:18 -0300

    stylying the movie selected page

--- src/components/pages/movies/movie.tsx
+++ src/components/pages/movies/movie.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 import api from '../../../services/api'
 import { Movie } from "../../../model/movie";
+import './move.scss';
 
 function Movies () {
   const { id } = useParams();
@@ -42,12 +43,21 @@ function Movies () {
 
   return (
     <div className="container">
+          <h1>{movieSelect!.title}</h1>
           <img src={`https://image.tmdb.org/t/p/original/${movieSelect!.backdrop_path}`} alt={movieSelect!.title} />
 
           <h3>Sinopse</h3>
           <span>{movieSelect!.overview}</span>
 
           <strong>Grade: {movieSelect!.vote_average} / 10</strong>
+
+          <div className="buttons">
+            <button>Save</button>
+            <button>
+              <a href="#">Trailer</a>
+
+            </button>
+          </div>
     </div>
   )
 }
