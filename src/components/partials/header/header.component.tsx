2d10e5103f0f25acddb5196b953891e2b9609288
Lucas Medeiros <lucas-medeiros@live.com>
2023-03-23 20:47:13 -0300

    adding sidebar

--- src/components/partials/header/header.component.tsx
+++ src/components/partials/header/header.component.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import './header.component.scss';
 import { Link } from 'react-router-dom';
+import { FaBars} from 'react-icons/fa';
 
 export default function Header(){
   const [query, setQuery] = useState("");
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSideBar = () => setSidebar(!sidebar);
 
   return (
     <header>
@@ -14,6 +18,9 @@ export default function Header(){
         <input type="text" placeholder='Search...' onChange={e => setQuery(e.target.value)} />
       </div>
       <Link className='favorite' to='/favorite'>MyMovies</Link>
+      <div className="sideBar">
+        <FaBars onClick={showSideBar} />
+      </div>
     </header>
   )
 }
---
+++ src/components/partials/sidebar/sidebar.component.tsx
@@ -0,0 +1,8 @@
+export function sideBar(){
+  const [genre, setGenre] = useState([]);
+  return (
+    <div className="sideBar">
+
+    </div>
+  )
+}
